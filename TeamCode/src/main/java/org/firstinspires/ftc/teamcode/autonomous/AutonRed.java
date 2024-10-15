c1d4affcd6f0f82ca08fc20805f9e82e9a7e1b80
Kavi-Gupta <69233899+Kavi-Gupta@users.noreply.github.com>
2024-10-14 18:43:46 -0700

    Fixed Servo Test Duplication added Autons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutonRed.java
@@ -5,7 +5,6 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.ParallelAction;
 import com.acmerobotics.roadrunner.Pose2d;
-import com.acmerobotics.roadrunner.QuinticSpline1d;
 import com.acmerobotics.roadrunner.SequentialAction;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
@@ -19,7 +18,7 @@ import org.firstinspires.ftc.teamcode.autonomous.subsystems.Wrist;
 @Config
 @Autonomous(name="Meet 0 Auton Both", group="Autonomous")
 
-public class Meet0AutonRed extends LinearOpMode {
+public class AutonRed extends LinearOpMode {
     @Override
     public void runOpMode() {
         Pose2d startPose = new Pose2d(12, -63.5, 90);
@@ -30,52 +29,31 @@ public class Meet0AutonRed extends LinearOpMode {
         Wrist wrist = new Wrist(hardwareMap);
         Intake intake = new Intake(hardwareMap);
 
-        TrajectoryActionBuilder main = drive.actionBuilder(startPose)
-                .strafeToLinearHeading(new Vector2d(0, -34), Math.toRadians(90))
-                .waitSeconds(2)
-                .strafeTo(new Vector2d(0, -36))
-                .waitSeconds(2)
-                .strafeTo(new Vector2d(-2, -36));
-
-        Action moveToSubmersible = drive.actionBuilder(startPose)
-                .strafeToLinearHeading(new Vector2d(0, -41), Math.toRadians(90))
+        Action approachSubmersible = drive.actionBuilder(startPose)
+                .strafeTo(new Vector2d(9, -39))
                 .build();
+                //move arm to specimen hanging position
 
-        Action reverseAndScoreInSubmersible = drive.actionBuilder(new Pose2d(0, -41, 90))
-                .waitSeconds(2)
-                .strafeTo(new Vector2d(0, -52))
-                .waitSeconds(2)
+        Action moveForwardToScoreSpecimen = drive.actionBuilder(new Pose2d(9, -39, 90))
+                .strafeTo(new Vector2d(9, -33))
                 .build();
+                //move arm down a little bit
 
 
-        Action moveLeftAfterEjection = drive.actionBuilder(new Pose2d(0, -52, 90))
-                .strafeTo(new Vector2d(-20, -56))
-                .build();
-
-        Action park = drive.actionBuilder(new Pose2d(-20, -56, 90))
-                .setTangent(0)
-                .splineToSplineHeading(new Pose2d(-34, -12, 0), Math.PI / 2)
-                .strafeTo(new Vector2d(-30, -12))
+        Action reverseAndScoreInSubmersible = drive.actionBuilder(new Pose2d(9, -33, 90))
+                .strafeTo(new Vector2d(9, -43))
                 .build();
 
         SequentialAction auto = new SequentialAction(
                 new ParallelAction(
-                        moveToSubmersible,
+                        approachSubmersible,
                         arm.liftToSpecimen()
                 ),
                 new ParallelAction(
-                        reverseAndScoreInSubmersible,
-                        arm.scoreSpecimen()
+                        arm.scoreSpecimen(),
+                        moveForwardToScoreSpecimen
                 ),
-                new ParallelAction(
-                        intake.deposit(),
-                        moveLeftAfterEjection
-                ),
-                new ParallelAction(
-                        arm.clearGround(),
-                        park
-                ),
-                arm.touchBottomBar()
+                reverseAndScoreInSubmersible
         );
 
         Actions.runBlocking(arm.clearGround());
